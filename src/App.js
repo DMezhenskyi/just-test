90b9fed59eccc251a6645c025fcbec09f5d79c30
Dmitriy Mezhenskiy <mezhik91@gmail.com>
2020-06-24 12:39:55 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { Wizard, Step, Controls } from "react-decision-tree-flow";
 
 function App() {
+  const tree = {
+    step1: ["step1", "step2"],
+    step2: ["step2", "step3", "step4"],
+    step3: ["step3", "step1"],
+    step4: ["step4", "step2"],
+  };
+  const [steps, setSteps] = useState([]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {steps.map(({ name, navigate }, index) => (
+          <button
+            onClick={() => {
+              setSteps(steps.splice(0, index));
+              navigate();
+            }}
+            key={index}
+          >
+            {name}
+          </button>
+        ))}
       </header>
+      <Wizard tree={tree} first="step1">
+        <Step name="step1">
+          <div className="step">
+            <h1>I am step 1</h1>
+            <br />
+            <Controls>
+              {({ destinations: { step2, step1 } }) => (
+                <button
+                  onClick={() => {
+                    setSteps([
+                      ...steps,
+                      { name: "back to step 1", navigate: step1 },
+                    ]);
+                    return step2();
+                  }}
+                >
+                  Go to Step 2
+                </button>
+              )}
+            </Controls>
+          </div>
+        </Step>
+        <Step name="step2">
+          <div className="step">
+            <h1>I am step 2</h1>
+            <br />
+            <Controls>
+              {({ destinations: { step3, step4, step2 } }) => (
+                <>
+                  <button
+                    onClick={() => {
+                      setSteps([
+                        ...steps,
+                        { name: "Back to step 2", navigate: step2 },
+                      ]);
+                      return step3();
+                    }}
+                  >
+                    Go to Step 3
+                  </button>
+                  <button
+                    onClick={() => {
+                      setSteps([
+                        ...steps,
+                        { name: "it is step 2", navigate: step2 },
+                      ]);
+                      return step4();
+                    }}
+                  >
+                    Go to Step 4
+                  </button>
+                </>
+              )}
+            </Controls>
+          </div>
+        </Step>
+        <Step name="step3">
+          <div className="step">
+            <h1>I am step 3</h1>
+            <br />
+            <Controls>
+              {({ destinations: { step4 }, back }) => (
+                <>
+                  <button onClick={step4}>Go to Step 4</button>
+                  <button onClick={back}>Go back</button>
+                </>
+              )}
+            </Controls>
+          </div>
+        </Step>
+      </Wizard>
     </div>
   );
 }
